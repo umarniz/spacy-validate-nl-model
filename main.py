81e8aff4e83fb83e0e1d074d6d58cdac8c72f756
Umar Nizamani <umar.nizamani@gmail.com>
2019-12-09 21:25:56 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from conllu import parse_incr
 import spacy
 
+# Load the spacy model
 nlp = spacy.load('nl_core_news_sm')
 print('Loaded Spacy Model')
 
-data_file = open("datasets/UD_Dutch-LassySmall/nl_lassysmall-ud-test.conllu", "r", encoding="utf-8")
+test_file = open("UD_Dutch-LassySmall/nl_lassysmall-ud-test.conllu", "r", encoding="utf-8")
 
 incorrect_sentences = 0
 correct_sentences = 0
@@ -18,11 +19,9 @@ def show_percentage():
     tokens_percent = float(correct_tokens) * 100.0/ float(incorrect_tokens + correct_tokens)
     print('------- Tokens    {0:.2f}% - Incorrect {1} vs correct {2}'.format(tokens_percent, incorrect_tokens, correct_tokens))
 
-for tokenlist in parse_incr(data_file):
-    # e.g OrderedDict([('id', 1), ('form', 'Totale'), ('lemma', 'totaal'), ('upostag', 'ADJ'), ('xpostag', 'ADJ|prenom|basis|met-e|stan'), ('feats', OrderedDict([('Degree', 'Pos')])), ('head', 2), ('deprel', 'amod'), ('deps', [('amod', 2)]), ('misc', None)])
-
-    # We create an array of flattened tokens to make sure we use the same data in the ground truth
-    # As is available in metdata.text
+for tokenlist in parse_incr(test_file):
+    # We create an array of flattened tokens to make sure we use the same data in the ground truth as is available in metadata.text
+    # We do this because the ground truth contains nested tokens that don't exist in the metadata text file
     flattened_tokens = []
 
     for token in tokenlist:
@@ -30,56 +29,59 @@ for tokenlist in parse_incr(data_file):
         if isinstance(token['id'], int):
             flattened_tokens.append(token)
 
-    # Lets work with sentences that are alteast 3 characters
-    tokencount = len(flattened_tokens)
-    # if tokencount < 3:
-    #     continue
+    # Parse the text in metadata object with Spacy
+    metdata_text = tokenlist.metadata['text']
+    spacy_tokens = nlp(metdata_text)
 
+    truth_token_count = len(flattened_tokens)
+    spacy_token_count = len(spacy_tokens)
+    
     token_index = 0
-    text = tokenlist.metadata['text']
-
-    spacy_tokens = nlp(text)
 
     has_mistake = False
-    for i in range(0, tokencount):
-        current_token = flattened_tokens[i]
+    for i in range(0, truth_token_count):
         
+        # For some sentences, spacy parses different number of tokens then the ground truth
+        # e.g not understanding the . in the end of a sentence is a punctuation and not part of the last word
+        # For these cases we have a special case to debug more information in the terminal
         if len(spacy_tokens) <= i:
             has_mistake = True
             incorrect_tokens += 1
 
-            spacy_text = []
+            spacy_token_words = []
             for token in spacy_tokens:
-                spacy_text.append(str(token))
+                spacy_token_words.append(str(token))
 
-            spacy_tags = []
+            spacy_token_tags = []
             for token in spacy_tokens:
-                spacy_tags.append(str(token.pos_))
+                spacy_token_tags.append(str(token.pos_))
 
-            truth_text = []
+            truth_token_words = []
             for token in flattened_tokens:
-                truth_text.append(str(token['form']))
+                truth_token_words.append(str(token['form']))
             
-            truth_tags = []
+            truth_token_tags = []
             for token in flattened_tokens:
-                truth_tags.append(str(token['upostag']))
+                truth_token_tags.append(str(token['upostag']))
             
-            print('\nMismatch of tokens. Truth {} Spacy {}'.format(tokencount, len(spacy_tokens)))
-            print('Truth ' + '-'.join(truth_text))
-            print('Spacy ' + '-'.join(spacy_text))
-            print('TruthTags ' + '-'.join(truth_tags))
-            print('SpacyTags ' + '-'.join(spacy_tags))
+            print('\nMismatch of parsed # of tokens. Truth had {} tokens but Spacy parsed the text into {} tokens'.format(truth_token_count, len(spacy_tokens)))
+            print('Truth ' + '-'.join(truth_token_words))
+            print('Spacy ' + '-'.join(spacy_token_words))
+            print('TruthTags ' + '-'.join(truth_token_tags))
+            print('SpacyTags ' + '-'.join(spacy_token_tags))
             break
 
+        truth_token = flattened_tokens[i]
         spacy_token = spacy_tokens[i]
+        
+        spacy_pos_tag = str(spacy_token.pos_)
+        truth_pos_tag = str(truth_token['upostag'])
 
         # If the POS token from spacy doesn't match match the ground truth we consider a mistake
-
-        # NOTE: We are using `in` instead of equals to for string matching because the ground truth 
+        # NOTE: We are using `in` instead of '=' for string matching because the ground truth 
         # contains SCONJ and CCONJ while spacy only returns CONJ for those tokens
-        if str(spacy_token.pos_) not in str(current_token['upostag']):
+        if spacy_pos_tag not in truth_pos_tag:
             has_mistake = True
-            # print('Truth {}-{} vs {}-{}'.format(current_token['form'], current_token['upostag'], spacy_token, spacy_token.pos_))
             incorrect_tokens += 1
         else:
             correct_tokens += 1
@@ -94,4 +96,4 @@ for tokenlist in parse_incr(data_file):
 
 show_percentage()
 
-data_file.close()
+test_file.close()
